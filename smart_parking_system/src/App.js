f414d774a02cf091e9360d00eee54a0cc46da1e6
Guruprasad2003 <gurubhatkodani@gmail.com>
2024-07-20 11:17:56 +0530

    footer added

--- smart_parking_system/src/App.js
+++ smart_parking_system/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home/Home';
-
+import './components/Home/Home.css';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <Home/>
+ 
+   <Home/>
   );
 }
-
 export default App;
--- smart_parking_system/src/components/Home/Home.jsx
+++ smart_parking_system/src/components/Home/Home.jsx
@@ -1,12 +1,41 @@
-import React  from 'react';
+import React, { useState }  from 'react';
+import { FaParking } from "react-icons/fa";
+import { useMediaQuery } from "react-responsive";
+import Toggle from "react-toggle";
 const Home =()=>{
   return (
-    <div>
+    <div class="home">
       <header>
-        <h1>Smart Parking System</h1>
-          
+
+        <h1><FaParking />SMART<br/>PARKING</h1>
+     
       </header>
+      
+      <main>
+        <p><b>Find  Your's Space to Park</b></p>
+        <p><b>Payment and Management</b></p>
+        <button>click me!</button>
+      </main>
+    
+
+
+     </div>
+
+  );
+} 
+
+function Home () {
+
+  const handleClick = () => {
+    alert('Button clicked!');
+  };
+
+  return (
+    <div>
+      <h1>Welcome to my website!</h1>
+      <button onClick={handleClick}>Click me!</button>
     </div>
   );
 }
+
 export default Home;
